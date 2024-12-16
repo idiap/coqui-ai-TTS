a32e36292389cf4d746e65c04fea06a2b5ea8459
Enno Hermann <enno.hermann@idiap.ch>
2024-12-16 22:04:36 +0100

    test(bin.extract_spectrograms): run cli test from python

--- tests/aux_tests/test_extract_tts_spectrograms.py
+++ tests/aux_tests/test_extract_tts_spectrograms.py
@@ -3,7 +3,8 @@ from pathlib import Path
 import pytest
 import torch
 
-from tests import get_tests_input_path, run_cli
+from tests import get_tests_input_path, run_main
+from TTS.bin.extract_tts_spectrograms import main
 from TTS.config import load_config
 from TTS.tts.models import setup_model
 
@@ -12,13 +13,11 @@ torch.manual_seed(1)
 
 @pytest.mark.parametrize("model", ["glow_tts", "tacotron", "tacotron2"])
 def test_extract_tts_spectrograms(tmp_path, model):
-    config_path = Path(get_tests_input_path()) / f"test_{model}_config.json"
-    checkpoint_path = tmp_path / f"{model}.pth"
-    output_path = tmp_path / "output_extract_tts_spectrograms"
+    config_path = str(Path(get_tests_input_path()) / f"test_{model}_config.json")
+    checkpoint_path = str(tmp_path / f"{model}.pth")
+    output_path = str(tmp_path / "output_extract_tts_spectrograms")
 
-    c = load_config(str(config_path))
-    model = setup_model(c)
+    config = load_config(config_path)
+    model = setup_model(config)
     torch.save({"model": model.state_dict()}, checkpoint_path)
-    run_cli(
-        f'CUDA_VISIBLE_DEVICES="" python TTS/bin/extract_tts_spectrograms.py --config_path "{config_path}" --checkpoint_path "{checkpoint_path}" --output_path "{output_path}"'
-    )
+    run_main(main, ["--config_path", config_path, "--checkpoint_path", checkpoint_path, "--output_path", output_path])
