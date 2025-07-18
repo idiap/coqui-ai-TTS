9d03b314e35019b5ef97f3557d1ec929fe5042e4
Enno Hermann <Eginhard@users.noreply.github.com>
2025-07-18 13:09:41 +0200

    fix(recipes): wrap scripts with main() functions (#462)

--- recipes/thorsten_DE/align_tts/train_aligntts.py
+++ recipes/thorsten_DE/align_tts/train_aligntts.py
@@ -13,72 +13,79 @@ from TTS.utils.downloaders import download_thorsten_de
 output_path = os.path.dirname(os.path.abspath(__file__))
 
 # init configs
-dataset_config = BaseDatasetConfig(
-    formatter="thorsten", meta_file_train="metadata.csv", path=os.path.join(output_path, "../thorsten-de/")
-)
 
-# download dataset if not already present
-if not os.path.exists(dataset_config.path):
-    print("Downloading dataset")
-    download_thorsten_de(os.path.split(os.path.abspath(dataset_config.path))[0])
 
-config = AlignTTSConfig(
-    batch_size=32,
-    eval_batch_size=16,
-    num_loader_workers=4,
-    num_eval_loader_workers=4,
-    run_eval=True,
-    test_delay_epochs=-1,
-    epochs=1000,
-    text_cleaner="multilingual_phoneme_cleaners",
-    use_phonemes=False,
-    phoneme_language="de",
-    phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
-    print_step=25,
-    print_eval=True,
-    mixed_precision=False,
-    test_sentences=[
-        "Es hat mich viel Zeit gekostet ein Stimme zu entwickeln, jetzt wo ich sie habe werde ich nicht mehr schweigen.",
-        "Sei eine Stimme, kein Echo.",
-        "Es tut mir Leid David. Das kann ich leider nicht machen.",
-        "Dieser Kuchen ist großartig. Er ist so lecker und feucht.",
-        "Vor dem 22. November 1963.",
-    ],
-    output_path=output_path,
-    datasets=[dataset_config],
-)
+def main():
+    dataset_config = BaseDatasetConfig(
+        formatter="thorsten", meta_file_train="metadata.csv", path=os.path.join(output_path, "../thorsten-de/")
+    )
 
-# INITIALIZE THE AUDIO PROCESSOR
-# Audio processor is used for feature extraction and audio I/O.
-# It mainly serves to the dataloader and the training loggers.
-ap = AudioProcessor.init_from_config(config)
+    # download dataset if not already present
+    if not os.path.exists(dataset_config.path):
+        print("Downloading dataset")
+        download_thorsten_de(os.path.split(os.path.abspath(dataset_config.path))[0])
 
-# INITIALIZE THE TOKENIZER
-# Tokenizer is used to convert text to sequences of token IDs.
-# If characters are not defined in the config, default characters are passed to the config
-tokenizer, config = TTSTokenizer.init_from_config(config)
+    config = AlignTTSConfig(
+        batch_size=32,
+        eval_batch_size=16,
+        num_loader_workers=4,
+        num_eval_loader_workers=4,
+        run_eval=True,
+        test_delay_epochs=-1,
+        epochs=1000,
+        text_cleaner="multilingual_phoneme_cleaners",
+        use_phonemes=False,
+        phoneme_language="de",
+        phoneme_cache_path=os.path.join(output_path, "phoneme_cache"),
+        print_step=25,
+        print_eval=True,
+        mixed_precision=False,
+        test_sentences=[
+            "Es hat mich viel Zeit gekostet ein Stimme zu entwickeln, jetzt wo ich sie habe werde ich nicht mehr schweigen.",
+            "Sei eine Stimme, kein Echo.",
+            "Es tut mir Leid David. Das kann ich leider nicht machen.",
+            "Dieser Kuchen ist großartig. Er ist so lecker und feucht.",
+            "Vor dem 22. November 1963.",
+        ],
+        output_path=output_path,
+        datasets=[dataset_config],
+    )
 
-# LOAD DATA SAMPLES
-# Each sample is a list of ```[text, audio_file_path, speaker_name]```
-# You can define your custom sample loader returning the list of samples.
-# Or define your custom formatter and pass it to the `load_tts_samples`.
-# Check `TTS.tts.datasets.load_tts_samples` for more details.
-train_samples, eval_samples = load_tts_samples(
-    dataset_config,
-    eval_split=True,
-    eval_split_max_size=config.eval_split_max_size,
-    eval_split_size=config.eval_split_size,
-)
+    # INITIALIZE THE AUDIO PROCESSOR
+    # Audio processor is used for feature extraction and audio I/O.
+    # It mainly serves to the dataloader and the training loggers.
+    ap = AudioProcessor.init_from_config(config)
 
-# init model
-model = AlignTTS(config, ap, tokenizer)
+    # INITIALIZE THE TOKENIZER
+    # Tokenizer is used to convert text to sequences of token IDs.
+    # If characters are not defined in the config, default characters are passed to the config
+    tokenizer, config = TTSTokenizer.init_from_config(config)
 
-# INITIALIZE THE TRAINER
-# Trainer provides a generic API to train all the 🐸TTS models with all its perks like mixed-precision training,
-# distributed training, etc.
-trainer = Trainer(
-    TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
-)
+    # LOAD DATA SAMPLES
+    # Each sample is a list of ```[text, audio_file_path, speaker_name]```
+    # You can define your custom sample loader returning the list of samples.
+    # Or define your custom formatter and pass it to the `load_tts_samples`.
+    # Check `TTS.tts.datasets.load_tts_samples` for more details.
+    train_samples, eval_samples = load_tts_samples(
+        dataset_config,
+        eval_split=True,
+        eval_split_max_size=config.eval_split_max_size,
+        eval_split_size=config.eval_split_size,
+    )
 
-# AND... 3,2,1... 🚀
-trainer.fit()
+    # init model
+    model = AlignTTS(config, ap, tokenizer)
+
+    # INITIALIZE THE TRAINER
+    # Trainer provides a generic API to train all the 🐸TTS models with all its perks like mixed-precision training,
+    # distributed training, etc.
+    trainer = Trainer(
+        TrainerArgs(), config, output_path, model=model, train_samples=train_samples, eval_samples=eval_samples
+    )
+
+    # AND... 3,2,1... 🚀
+    trainer.fit()
+
+
+if __name__ == "__main__":
+    main()
