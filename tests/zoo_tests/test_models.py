52102ac7f8b7b9f0aa2b54421c2348401d50c067
Enno Hermann <enno.hermann@idiap.ch>
2025-12-21 18:43:34 +0100

    test(zoo): move zoo tests to Python API

--- tests/zoo_tests/test_models.py
+++ tests/zoo_tests/test_models.py
@@ -7,8 +7,6 @@ import pytest
 from tests import get_tests_data_path, run_main
 from TTS.api import TTS
 from TTS.bin.synthesize import main
-from TTS.tts.utils.languages import LanguageManager
-from TTS.tts.utils.speakers import SpeakerManager
 from TTS.utils.manage import ModelManager
 
 MODELS_WITH_SEP_TESTS = [
@@ -34,12 +32,6 @@ def run_around_tests(tmp_path):
     shutil.rmtree(tmp_path)
 
 
-@pytest.fixture
-def manager(tmp_path):
-    """Set up model manager."""
-    return ModelManager(output_prefix=tmp_path, progress_bar=False)
-
-
 # To split tests into different CI jobs
 num_partitions = int(os.getenv("NUM_PARTITIONS", "1"))
 partition = int(os.getenv("TEST_PARTITION", "0"))
@@ -49,42 +41,23 @@ model_names = [name for i, name in enumerate(model_names) if i % num_partitions 
 
 
 @pytest.mark.parametrize("model_name", model_names)
-def test_models(tmp_path, model_name, manager):
+def test_models(tmp_path, model_name):
     print(f"\n > Run - {model_name}")
-    output_path = str(tmp_path / "output.wav")
-    model_path, _, _ = manager.download_model(model_name)
-    args = ["--model_name", model_name, "--out_path", output_path, "--no-progress_bar"]
+    api = TTS(model_name)
+    file_path = tmp_path / "output.wav"
     if "tts_models" in model_name:
-        local_download_dir = model_path.parent
-        # download and run the model
-        speaker_files = list(local_download_dir.glob("speaker*"))
-        language_files = list(local_download_dir.glob("language*"))
-        speaker_arg = []
-        language_arg = []
-        if len(speaker_files) > 0:
-            # multi-speaker model
-            if "speaker_ids" in speaker_files[0].stem:
-                speaker_manager = SpeakerManager(speaker_id_file_path=speaker_files[0])
-            elif "speakers" in speaker_files[0].stem:
-                speaker_manager = SpeakerManager(d_vectors_file_path=speaker_files[0])
-            speakers = list(speaker_manager.name_to_id.keys())
-            if len(speakers) > 1:
-                speaker_arg = ["--speaker_idx", speakers[0]]
-        if len(language_files) > 0 and "language_ids" in language_files[0].stem:
-            # multi-lingual model
-            language_manager = LanguageManager(language_ids_file_path=language_files[0])
-            languages = language_manager.language_names
-            if len(languages) > 1:
-                language_arg = ["--language_idx", languages[0]]
-        run_main(main, [*args, "--text", "This is an example.", *speaker_arg, *language_arg])
+        speaker = None
+        language = api.languages[0] if api.languages else None
+        speaker = api.speakers[0] if api.speakers else None
+        api.tts_to_file("This is an example.", speaker=speaker, language=language, file_path=file_path)
     elif "voice_conversion_models" in model_name:
         speaker_wav = os.path.join(get_tests_data_path(), "ljspeech", "wavs", "LJ001-0001.wav")
         reference_wav1 = os.path.join(get_tests_data_path(), "ljspeech", "wavs", "LJ001-0028.wav")
         reference_wav2 = os.path.join(get_tests_data_path(), "ljspeech", "wavs", "LJ001-0032.wav")
-        run_main(main, [*args, "--source_wav", speaker_wav, "--target_wav", reference_wav1, reference_wav2])
+        api.voice_conversion_to_file(speaker_wav, [reference_wav1, reference_wav2], file_path=file_path)
     else:
         # only download the model
-        manager.download_model(model_name)
+        ModelManager(output_prefix=tmp_path, progress_bar=False).download_model(model_name)
     print(f" | > OK: {model_name}")
 
 
