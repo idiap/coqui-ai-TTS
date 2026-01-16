6b9dafb8e974b0dec72dd3c968f43ec76456f88a
Enno Hermann <enno.hermann@idiap.ch>
2026-01-16 10:01:00 +0100

    TMP: xtts manager

--- TTS/tts/layers/xtts/xtts_manager.py
+++ TTS/tts/layers/xtts/xtts_manager.py
@@ -1,20 +1,23 @@
+import os
+from typing import Any
+
 import torch
 
 from TTS.utils.generic_utils import is_pytorch_at_least_2_4
 
 
 class SpeakerManager:
-    def __init__(self, speaker_file_path=None):
+    def __init__(self, speaker_file_path: str | os.PathLike[Any]) -> None:
         self.speakers = torch.load(speaker_file_path, weights_only=is_pytorch_at_least_2_4())
 
     @property
-    def name_to_id(self):
+    def name_to_id(self) -> dict[str, dict[str, torch.Tensor]]:
         return self.speakers
 
     @property
-    def num_speakers(self):
+    def num_speakers(self) -> int:
         return len(self.name_to_id)
 
     @property
-    def speaker_names(self):
+    def speaker_names(self) -> list[str]:
         return list(self.name_to_id.keys())
